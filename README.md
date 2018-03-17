0d827f13c725cfb4407ec1f6254b3f8df26abf43
Amit Prasad <amitprasad53121@gmail.com>
2018-03-17 22:05:09 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Text Search Engine
+
+CA towards CS7IS3-A-Y-201718-Information-Retrieval-And-Web-Search
---
+++ src/main/java/com/ir/searchengine/Application.java
@@ -0,0 +1,8 @@
+package com.ir.searchengine;
+
+public class Application {
+
+	public static void main(String args[]) {
+		
+	}
+}
