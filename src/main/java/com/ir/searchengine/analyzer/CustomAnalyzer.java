a5c4aa34984d5aabeaca8c63bd9964848ab57968
YuhaoQiao <33913866+YuhaoQiao@users.noreply.github.com>
2018-03-22 15:29:04 +0000

    Add files via upload
    -m "custom analyser using basic stemmers"

--- src/main/java/com/ir/searchengine/analyzer/CustomAnalyzer.java
+++ src/main/java/com/ir/searchengine/analyzer/CustomAnalyzer.java
@@ -1,11 +1,16 @@
-package com.ir.searchengine.analyzer;
+package com.ir.searchengine.Analyzer;
 
-import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.analysis.LowerCaseFilter;
 import org.apache.lucene.analysis.StopFilter;
+import org.apache.lucene.analysis.StopwordAnalyzerBase;
 import org.apache.lucene.analysis.TokenFilter;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.analysis.en.EnglishPossessiveFilter;
+import org.apache.lucene.analysis.en.KStemFilter;
+import org.apache.lucene.analysis.en.PorterStemFilter;
 import org.apache.lucene.analysis.standard.StandardFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 
@@ -16,17 +21,27 @@ import com.ir.searchengine.filters.SpecialCharFilter;
  * 
  * @author amit
  */
-public class CustomAnalyzer extends Analyzer {
+public class CustomAnalyzer extends StopwordAnalyzerBase {
 
-	@Override
-	protected TokenStreamComponents createComponents(String fieldName) {
+    /**
+     * Builds the Analyzer
+     * @param stopwords list of stopwords
+     */
+    public CustomAnalyzer() {
 
-		final StandardTokenizer src = new StandardTokenizer();
-		TokenStream tok = new StandardFilter(src);
-		TokenFilter filter = new SpecialCharFilter(tok);
-		filter = new LowerCaseFilter(filter);
-		filter = new StopFilter(filter, StopAnalyzer.ENGLISH_STOP_WORDS_SET);
-		return new TokenStreamComponents(src, filter);
-	}
+    }
+    
+    @Override
+    protected TokenStreamComponents createComponents(String fieldName) {
+    	Tokenizer tokenizer = new StandardTokenizer();
+        TokenStream tokenStream = new StandardFilter(tokenizer);
+        tokenStream = new LowerCaseFilter(tokenStream);
+        tokenStream = new EnglishPossessiveFilter(tokenStream);
+        tokenStream = new StopFilter(tokenStream, stopwords);
+        tokenStream = new KStemFilter(tokenStream);
+        tokenStream = new PorterStemFilter(tokenStream);
+        TokenStreamComponents tokenStreamComponents = new TokenStreamComponents(tokenizer, tokenStream);
+        return tokenStreamComponents;
+    }
 
 }
