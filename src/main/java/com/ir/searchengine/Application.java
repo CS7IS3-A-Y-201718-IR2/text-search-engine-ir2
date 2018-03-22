75b4a26fc619c6176f8ab8f8feb26853c009feeb
Amit Prasad <amitprasad53121@gmail.com>
2018-03-22 21:25:26 +0000

    fire query implemented

--- src/main/java/com/ir/searchengine/Application.java
+++ src/main/java/com/ir/searchengine/Application.java
@@ -1,5 +1,8 @@
 package com.ir.searchengine;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,14 +12,23 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.apache.log4j.Logger;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.search.similarities.Similarity;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import org.xml.sax.SAXException;
 
 import com.ir.searchengine.analyzer.CustomAnalyzer;
+import com.ir.searchengine.constants.Constants;
 import com.ir.searchengine.indexing.IndexDocuments;
 import com.ir.searchengine.query.TopicDto;
 import com.ir.searchengine.query.XMLQueryParser;
@@ -30,21 +42,33 @@ public class Application {
 		Application app = new Application();
 
 		try {
-			//Analyzer analyzer = new CustomAnalyzer();
+			Directory dir = FSDirectory.open(Paths.get(Constants.INDEX_DIR));
+
+			// Analyzer analyzer = new CustomAnalyzer();
 			Analyzer analyzer = new StandardAnalyzer();
 			Similarity similarity = new BM25Similarity();
 
 			List<TopicDto> topics = app.loadTopics();
 			List<Query> queries = app.createQuery(analyzer, topics);
 			
-			IndexDocuments idx = new IndexDocuments(analyzer, similarity);
-			idx.indexFbisDocs();
-//			idx.indexFr94Docs();
-//			idx.indexFtDocs();
-//			idx.indexLatTimeDocs();
+			// If the directory exists then do not recreate indexes
+			if (!new File(Constants.INDEX_DIR).exists()) {
+				
+				// create the directory
+				new File(Constants.INDEX_DIR).mkdirs();
+				
+				IndexDocuments idx = new IndexDocuments(analyzer, similarity);
+				idx.indexFbisDocs();
+				idx.indexFr94Docs();
+				idx.indexFtDocs();
+				idx.indexLatTimeDocs();
+			}
+
+			for (Query query : queries)
+				app.fireQuery(query, dir, similarity, 10);
 
 		} catch (Exception e) {
-			logger.error(e.getMessage(),e);
+			logger.error(e.getMessage(), e);
 			System.exit(-1);
 		}
 	}
@@ -73,8 +97,8 @@ public class Application {
 		List<Query> queries = new ArrayList<>();
 
 		for (TopicDto topic : topics) {
-			MultiFieldQueryParser multiFieldQP = new MultiFieldQueryParser(
-					new String[] {"content" }, analyzer, boosts);
+			MultiFieldQueryParser multiFieldQP = new MultiFieldQueryParser(new String[] { "content" }, analyzer,
+					boosts);
 			Query q;
 			try {
 				q = multiFieldQP.parse(topic.getDesc());
@@ -85,5 +109,42 @@ public class Application {
 		}
 
 		return queries;
+	}
+
+	public ScoreDoc[] fireQuery(Query query, Directory dir, Similarity similarity, int hitsPerPage) {
+
+		try (IndexReader reader = DirectoryReader.open(dir)) {
+			IndexSearcher searcher = new IndexSearcher(reader);
+
+			// Scoring - Similarity
+			searcher.setSimilarity(similarity);
+
+			TopDocs docs = searcher.search(query, hitsPerPage);
+			ScoreDoc[] hits = docs.scoreDocs;
+
+			// display results
+			boolean printResults = true;
+			if (printResults) {
+				System.out.println("Found " + hits.length + " hits.");
+				System.err.print("\nRES_NO | DOCID | TITLE | SCORE\n");
+				for (int i = 0; i < hits.length; ++i) {
+					int docId = hits[i].doc;
+					double score = hits[i].score;
+					Document d = searcher.doc(docId);
+					System.out.println((i + 1) + ". " + d.get("docId") + "\t" + d.get("title") + "\t" + score);
+				}
+			}
+
+			// reader can only be closed when there
+			// is no need to access the documents any more.
+			reader.close();
+
+			return hits;
+		} catch (IOException e) {
+			logger.error("Failed to execute query '" + query + "'", e);
+		}
+
+		return null;
+
 	}
 }
